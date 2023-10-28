5ba083a3e8171067a730dbe348c21bb25c8d4321
Tesimune <tesimune@gmail.com>
2023-10-28 21:49:32 +0100

    Mobile view update

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    // errorElement: <ErrorPage />,
   },
   {
     path: "/chatgpt",
--- src/pages/ChatGPT.jsx
+++ src/pages/ChatGPT.jsx
@@ -71,7 +71,7 @@ export default function ChatGPT() {
     setActive(false);
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-9 px-20">
+    <main className="grid lg:flex min-h-screen flex-col items-center lg:justify-between py-3 lg:py-9 px-3 lg:px-20">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Link to="/" className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started with Sampic&nbsp;
